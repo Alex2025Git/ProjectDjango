8aba0cb82de013d60d068e74184219243d5932fd
unknown <gross3000@yandex.ru>
2025-11-15 01:08:08 +0300

    add project Django,add app catalog

---
+++ catalog/views.py
@@ -0,0 +1,21 @@
+import time
+
+from django.shortcuts import render
+
+
+def main(request):
+    return render(request, 'main.html')
+
+def contacts(request):
+    if request.method == "POST":
+        name = request.POST['name']
+        email = request.POST['email']
+        message = request.POST['messageInput']
+        time.sleep(2)
+    return render(request, 'contacts.html')
+
+def catalogs(request):
+    return render(request, 'catalogs.html')
+
+def category(request):
+    return render(request, 'category.html')
