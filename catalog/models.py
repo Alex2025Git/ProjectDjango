95896ccb1dbac1770e74923428ec586d17d04630
unknown <gross3000@yandex.ru>
2025-12-01 05:21:11 +0300

    correction migration class Product add category ForeignKey

--- catalog/models.py
+++ catalog/models.py
@@ -1,10 +1,22 @@
 from django.db import models
 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
 class Product(models.Model):
     name = models.CharField(max_length=100)
-    description = models.FloatField()
+    description = models.TextField()
     photo = models.ImageField()
-    category = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, related_name='products')
     price = models.FloatField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -13,19 +25,6 @@ class Product(models.Model):
         verbose_name = 'Product'
         verbose_name_plural = 'Products'
         ordering = ['created_at']
-
-    def __str__(self):
-        return self.name
-
-
-class Category(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.FloatField()
-
-    class Meta:
-        verbose_name = 'Category'
-        verbose_name_plural = 'Categories'
-        ordering = ['name']
 
     def __str__(self):
         return self.name
