a698d0857ce28d722ddea2fcafd81897c20b5cdf
unknown <gross3000@yandex.ru>
2026-01-19 13:06:20 +0300

    feat(app catalog) add services.py

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
+
 from catalog.apps import CatalogConfig
 from catalog.views import (MainTemplateView, ContactsTemplateView, CatalogsTemplateView, CategoryListView, ProductListView,
                            ProductDetailView, ProductCreateView, ProductUpdateView, ProductDeleteView)
@@ -12,9 +14,9 @@ urlpatterns = [
     path("home/", MainTemplateView.as_view(), name="main"),
     path("contacts/", ContactsTemplateView.as_view(), name="contacts"),
     path("catalogs/", CatalogsTemplateView.as_view(), name="catalogs"),
-    path("category/", CategoryListView.as_view(), name="category_list"),
-    path("categories/<int:category_id>/", ProductListView.as_view(), name="product_list"),
-    path('products/<int:pk>/', ProductDetailView.as_view(), name="product_detail"),
+    path("category/", cache_page(60)(CategoryListView.as_view()), name="category_list"),
+    path("categories/<int:category_id>/", cache_page(60)(ProductListView.as_view()), name="product_list"),
+    path('products/<int:pk>/', cache_page(60)(ProductDetailView.as_view()), name="product_detail"),
     path('products/create/', ProductCreateView.as_view(), name="product_create"),
     path('products/<int:pk>/update/', ProductUpdateView.as_view(), name="product_update"),
     path('products/<int:pk>/delete/', ProductDeleteView.as_view(), name="product_delete"),
--- config/settings.py
+++ config/settings.py
@@ -152,3 +152,12 @@ EMAIL_HOST_USER = os.getenv('MAIL_EMAIL_HOST_USER')
 
 LOGIN_URL = "users:login"
 
+CACHE_ENABLED = True
+if CACHE_ENABLED:
+    CACHES = {
+        "default": {
+            "BACKEND": "django.core.cache.backends.redis.RedisCache",
+            "LOCATION": "redis://127.0.0.1:6379/1"
+        }
+    }
+
